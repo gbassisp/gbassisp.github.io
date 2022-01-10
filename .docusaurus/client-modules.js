3eee0f997ca15e0452c2cc86c7637cddc0398287
gbassisp <guilherme.bassi.sp@gmail.com>
2022-01-10 21:41:06 +1030

    Removed .docusaurus from version control

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/gbassisp/dev/gbassisp.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/gbassisp/dev/gbassisp.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/gbassisp/dev/gbassisp.github.io/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/Users/gbassisp/dev/gbassisp.github.io/src/css/custom.css"),
-];
