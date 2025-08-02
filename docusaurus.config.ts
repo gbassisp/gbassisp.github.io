3e3ea0f3d54faad3d175f8c07f22874a885d9335
guilherme <guilherme.bassi.sp@gmail.com>
2025-08-02 20:56:21 +0930

    fix: minor adjustments

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/logo.png',
 
   // Set the production url of your site here
-  url: 'https://gbassisp.github.io',
+  url: 'https://el-darto.net',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -87,7 +87,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Projects, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} el-darto.net<br/>Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
