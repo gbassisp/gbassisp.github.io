2baf547c374c4a10f4a9506d40e515fa002fc0c6
guilherme <guilherme.bassi.sp@gmail.com>
2026-04-15 15:03:48 +0930

    content: added contact page

--- bin/main.dart
+++ bin/main.dart
@@ -17,6 +17,7 @@ Future<void> main(List<String> arguments) async {
   final staticShock = StaticShock()
     // Here, you can directly hook into the StaticShock pipeline. For example,
     // you can copy an "images" directory from the source set to build set:
+    ..pick(DirectoryPicker.parse('assets'))
     ..pick(DirectoryPicker.parse('images'))
     // All 3rd party behavior is added through plugins, even the behavior
     // shipped with Static Shock.
