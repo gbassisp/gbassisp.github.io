13ade50b8846696ba0bdde39171ebf36b8853fa6
guilherme <guilherme.bassi.sp@gmail.com>
2026-04-22 15:20:06 +0930

    feat: added humans.txt

--- bin/main.dart
+++ bin/main.dart
@@ -5,6 +5,7 @@ import 'package:static_shock/static_shock.dart';
 import 'correct_redirect_plugin.dart';
 import 'required_metadata_plugin.dart';
 import 'sitemap_plugin.dart';
+import 'utils.dart';
 
 Future<void> main(List<String> arguments) async {
   const baseUrl = 'https://el-darto.net/';
@@ -16,10 +17,7 @@ Future<void> main(List<String> arguments) async {
   );
   // Configure the static website generator.
   final staticShock = StaticShock()
-    // Here, you can directly hook into the StaticShock pipeline. For example,
-    ..pick(DirectoryPicker.parse('.well-known'))
-    ..pick(DirectoryPicker.parse('assets'))
-    ..pick(DirectoryPicker.parse('images'))
+    ..pickDefault()
     // All 3rd party behavior is added through plugins, even the behavior
     // shipped with Static Shock.
     ..plugin(const MarkdownPlugin())
